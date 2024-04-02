44948ccd354e51b00b4231050e3e0ad34b6621dd
Harshal Gidh <harshalgidh28102@gmail.com>
2024-04-02 18:23:29 +0530

    Model Evaluation Completed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from TextSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummarizer.logging import logger
 from TextSummarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from TextSummarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
+from TextSummarizer.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
+from TextSummarizer.pipeline.stage05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,11 +15,46 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
 STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
